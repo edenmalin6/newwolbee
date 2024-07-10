f70c911114219038f5de9b73144468ce8a5bff43
benAloni <146990293+benAloni@users.noreply.github.com>
2024-07-10 16:49:30 +0300

    Update Login.jsx

--- fronted/src/views/pages/Authentication/Login.jsx
+++ fronted/src/views/pages/Authentication/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://newwolbee-1.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
